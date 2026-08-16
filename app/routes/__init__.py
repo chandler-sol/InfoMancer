69c3af1fc618e95df1aec8e713da3fa2b615de02
arsenik <64607629+chandler-sol@users.noreply.github.com>
2026-08-16 01:59:35 -0400

    Register inspector media routes

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -5,6 +5,7 @@ from .operations import build_router as build_operations_router
 from .dashboard import build_router as build_dashboard_router
 from .review import build_router as build_review_router
 from .library import build_router as build_library_router
+from .inspector_media import build_router as build_inspector_media_router
 from .settings import build_router as build_settings_router
 from .collections import build_router as build_collections_router
 from .titles import build_router as build_titles_router
@@ -15,6 +16,7 @@ ROUTER_BUILDERS = (
     build_dashboard_router,
     build_review_router,
     build_library_router,
+    build_inspector_media_router,
     build_settings_router,
     build_collections_router,
     build_titles_router,
