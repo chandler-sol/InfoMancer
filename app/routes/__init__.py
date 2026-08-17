f81b0c652dfb127ff852d587a28f0ff0a4c8cb2e
arsenik <64607629+chandler-sol@users.noreply.github.com>
2026-08-17 14:00:51 -0400

    Prioritize static Scheduled Tasks route

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -7,8 +7,8 @@ from .review import build_router as build_review_router
 from .library import build_router as build_library_router
 from .inspector_media import build_router as build_inspector_media_router
 from .recovery import build_router as build_recovery_router
-from .settings import build_router as build_settings_router
 from .scheduled_tasks import build_router as build_scheduled_tasks_router
+from .settings import build_router as build_settings_router
 from .collections import build_router as build_collections_router
 from .titles import build_router as build_titles_router
 
@@ -20,8 +20,8 @@ ROUTER_BUILDERS = (
     build_library_router,
     build_inspector_media_router,
     build_recovery_router,
-    build_settings_router,
     build_scheduled_tasks_router,
+    build_settings_router,
     build_collections_router,
     build_titles_router,
 )
