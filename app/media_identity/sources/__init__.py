11eebc0f8b1247817c836ef72a5392b0037c0c51
arsenik <64607629+chandler-sol@users.noreply.github.com>
2026-09-19 23:16:13 -0400

    Export Jellyfin Trickplay tile helpers

--- app/media_identity/sources/__init__.py
+++ app/media_identity/sources/__init__.py
@@ -2,20 +2,30 @@
 
 from .jellyfin import (
     JellyfinAdapterError,
+    JellyfinTrickplayAsset,
     JellyfinTrickplayVariant,
+    crop_trickplay_frame,
     enumerate_preview_frames,
     external_paths_equal,
+    fetch_trickplay_tile,
     parse_trickplay_variants,
+    read_trickplay_preview,
     resolve_media_ref,
     select_trickplay_variant,
+    trickplay_tile_url,
 )
 
 __all__ = [
     "JellyfinAdapterError",
+    "JellyfinTrickplayAsset",
     "JellyfinTrickplayVariant",
+    "crop_trickplay_frame",
     "enumerate_preview_frames",
     "external_paths_equal",
+    "fetch_trickplay_tile",
     "parse_trickplay_variants",
+    "read_trickplay_preview",
     "resolve_media_ref",
     "select_trickplay_variant",
+    "trickplay_tile_url",
 ]
